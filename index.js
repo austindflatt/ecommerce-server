f9d1edca7d11dc45b91820364358b3a7f26e4028
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-04-25 15:47:58 -0400

    started building routes

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('hello')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+// const authRoute = require('./routes/auth');
+// const usersRoute = require('./routes/users');
+// const productsRoute = require('./routes/products');
+// const transactionsRoute = require('./routes/transactions');
+
+dotenv.config();
+
+mongoose
+	.connect(process.env.MONGODB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('MongoDB Connected'))
+	.catch((err) => {
+		console.log(err)
+	});
+
+app.use(express.json());
+// app.use('/api/auth', authRoute);
+// app.use('/api/users', usersRoute);
+// app.use('/api/products', productsRoute);
+// app.use('/api/transactions', transactionsRoute);
+
+app.listen(3001, () => {
+	console.log('Server is running...')
+})
