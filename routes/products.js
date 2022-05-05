74dd4b2fdb8c8cfbc606cfff3173e30431c2326e
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-05-05 12:48:45 -0400

    updated route for updating product

--- routes/products.js
+++ routes/products.js
@@ -25,7 +25,7 @@ router.put('/update/:id', verify, async (req, res) => {
     try {
       const updatedProduct = await Product.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
       const allProducts = await Product.find();
-      return res.status(200).json(updatedProduct);
+      return res.status(200).json(allProducts);
     } catch (error) {
       return res.status(500).json(error)
     }
